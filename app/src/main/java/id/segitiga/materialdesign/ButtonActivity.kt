d548e48edc93de47ae589f71cdf04295ea344d59
Misdi <misdi@outlook.com>
2019-11-17 23:13:27 +0700

    Initial commit

---
+++ app/src/main/java/id/segitiga/materialdesign/ButtonActivity.kt
@@ -0,0 +1,12 @@
+package id.segitiga.materialdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ButtonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button)
+    }
+}
